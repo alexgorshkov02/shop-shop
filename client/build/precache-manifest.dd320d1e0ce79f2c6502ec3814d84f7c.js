6a1db908c249cb5b5dc63434e99ddbc397d7a67b
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-09-17 19:14:57 -0400

    Fixed dependencies

---
+++ client/build/precache-manifest.dd320d1e0ce79f2c6502ec3814d84f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a86197ad92b6e6040864bbf98e01ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba52fa35b6fe91823d1d",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "ad6ef1e4c2b799676e58",
+    "url": "/static/js/2.d0d74202.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.d0d74202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba52fa35b6fe91823d1d",
+    "url": "/static/js/main.a2870328.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
